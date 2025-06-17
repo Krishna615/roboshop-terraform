7759439fa172038825d2211790e382bafe2dbae4
krish <krishnagelli4@gmail.com>
2025-06-17 13:40:06 -0400

    terraform commits

--- frontend.tf
+++ frontend.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "frontend-dev"{
+resource "aws_instance" "frontend"{
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-087a8efc6ad6a9899"]
