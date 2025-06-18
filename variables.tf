de305709086c930ee038302263c34d5eaa4af9ad
krish <krishnagelli4@gmail.com>
2025-06-18 13:05:07 -0400

    terraform commits

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "instances" {
       ami_id = "ami-09c813fb71547fc4f"
       instance_type = "t2.micro"
       vpc_security_group_ids = ["sg-087a8efc6ad6a9899"]
-    },
+    }
     mongodb = {
       ami_id = "ami-09c813fb71547fc4f"
       instace_type = "t2.small"
       vpc_security_group_ids = ["sg-087a8efc6ad6a9899"]
-    },
+    }
     catalogue = {
       ami_id = "ami-09c813fb71547fc4f"
       instance_type = "t3.small"
