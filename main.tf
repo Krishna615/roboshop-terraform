a3e54a09a58b6af71ea379fab4e94fa8da297c9a
krish <krishnagelli4@gmail.com>
2025-06-18 11:47:32 -0400

    ansible final commits

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "instance"{
+  for_each = var.instance
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags = {
+    Name = each.key
+  }
+
+}
+resource "aws_route53_record" "catalogue" {
+  for_each = var.instance
+  zone_id = var.zone_id
+  name    = "${each.key} - ${[var.env]}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance[each.key].private_ip]
+}
+# resource "null_resource" "catalogue" {
+#   provisioner "remote-exec" {
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.catalogue.private_ip
+#   }
+#
+#
+#     inline = [
+#       "sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost -U https://github.com/Krishna615/roboshop-ansible.git roboshop.yml -e component_name=catalogue -e env=dev",
+#     ]
+#   }
+
+#}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ami" {
+  default = "ami-09c813fb71547fc4f"
+}
+variable "instance_type" {
+  default= "t2.micro"
+}
+variable "vpc_security_group_ids" {
+  default = "sg-087a8efc6ad6a9899"
+}
+variable "instance" {
+  default = {
+    catalogue = null,
+    frontend = null,
+    mongodb = null
+  }
+}
+variable "zone_id" {
+  default = "Z02373281TX1LHFU7BPXU"
+}
+variable "env" {
+  default = "dev"
+}
