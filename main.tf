66ccc955d27de9d25187e7d6d88ad88ca7d85909
krish <krishnagelli4@gmail.com>
2025-06-18 11:56:02 -0400

    terraform commits

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "instance"{
   }
 
 }
-resource "aws_route53_record" "catalogue" {
+resource "aws_route53_record" "record" {
   for_each = var.instance
   zone_id = var.zone_id
   name    = "${each.key} - ${var.env}"
